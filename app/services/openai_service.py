6393034fc9cf1f61affab440e58e0340911babe3
Sudarshan S <senthilsudarshan@gmail.com>
2024-02-25 15:55:17 +0700

    gpt complete

--- app/services/openai_service.py
+++ app/services/openai_service.py
@@ -3,6 +3,7 @@ import shelve
 from dotenv import load_dotenv
 import os
 import time
+import logging
 import json
 
 load_dotenv()
@@ -13,7 +14,6 @@ client = OpenAI(api_key=OPENAI_API_KEY)
 # JSON file with gpt instructions
 with open('instructions.json') as f:
     instructions = json.load(f)
-
 
 def create_assistant():
     assistant = client.beta.assistants.create(
@@ -26,10 +26,7 @@ def create_assistant():
     print(f'Assistant ID: {assistant.id}')
     return assistant
 
-
-assistant = create_assistant()
-
-# manage chat threads using shelve
+# Use context manager to ensure the shelf file is closed properly
 def check_if_thread_exists(wa_id):
     with shelve.open("threads_db") as threads_shelf:
         return threads_shelf.get(wa_id, None)
@@ -39,21 +36,46 @@ def store_thread(wa_id, thread_id):
     with shelve.open("threads_db", writeback=True) as threads_shelf:
         threads_shelf[wa_id] = thread_id
 
-# Responses 
+
+def run_assistant(thread, name):
+    # Retrieve the Assistant
+    assistant = client.beta.assistants.retrieve(OPENAI_ASSISTANT_ID)
+
+    # Run the assistant
+    run = client.beta.threads.runs.create(
+        thread_id=thread.id,
+        assistant_id=assistant.id,
+        # instructions=f"You are having a conversation with {name}",
+    )
+
+    # Wait for completion
+    # https://platform.openai.com/docs/assistants/how-it-works/runs-and-run-steps#:~:text=under%20failed_at.-,Polling%20for%20updates,-In%20order%20to
+    while run.status != "completed":
+        # Be nice to the API
+        time.sleep(0.5)
+        run = client.beta.threads.runs.retrieve(thread_id=thread.id, run_id=run.id)
+
+    # Retrieve the Messages
+    messages = client.beta.threads.messages.list(thread_id=thread.id)
+    new_message = messages.data[0].content[0].text.value
+    logging.info(f"Generated message: {new_message}")
+    return new_message
+
+
 def generate_response(message_body, wa_id, name):
     # Check if there is already a thread_id for the wa_id
     thread_id = check_if_thread_exists(wa_id)
 
     # If a thread doesn't exist, create one and store it
     if thread_id is None:
-        print(f"Creating new thread for {name} with wa_id {wa_id}")
+        logging.info(f"Creating new thread for {name} with wa_id {wa_id}")
         thread = client.beta.threads.create()
         store_thread(wa_id, thread.id)
         thread_id = thread.id
 
     # Otherwise, retrieve the existing thread
     else:
-        print(f"Retrieving existing thread for {name} with wa_id {wa_id}")
+        logging.info(f"Retrieving existing thread for {name} with wa_id {wa_id}")
         thread = client.beta.threads.retrieve(thread_id)
 
     # Add message to thread
@@ -64,31 +86,6 @@ def generate_response(message_body, wa_id, name):
     )
 
     # Run the assistant and get the new message
-    new_message = run_assistant(thread)
-    print(f"To {name}:", new_message)
+    new_message = run_assistant(thread, name)
+
     return new_message
-
-
-def run_assistant(thread):
-  # Retrieve the Assistant
-  assistant = client.beta.assistants.retrieve(OPENAI_ASSISTANT_ID)
-
-  # Run the assistant
-  run = client.beta.threads.runs.create(
-      thread_id=thread.id,
-      assistant_id=assistant.id,
-  )
-
-  # Wait for completion
-  while run.status != "completed":
-      # Be nice to the API
-      time.sleep(0.5)
-      run = client.beta.threads.runs.retrieve(thread_id=thread.id, run_id=run.id)
-
-  # Retrieve the Messages
-  messages = client.beta.threads.messages.list(thread_id=thread.id)
-  new_message = messages.data[0].content[0].text.value
-  print(f"Generated message: {new_message}")
-  return new_message
-
-new_message = generate_response("chocolate cake.", "123", "John")
